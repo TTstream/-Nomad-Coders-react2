afd898c956adb0d8ee8654dcdc549f43954b69f2
SeongJin Oh <guk0230@naver.com>
2023-04-04 14:13:34 +0900

    ToDoList_리스트 누적

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  // Cleanup 1번째 방법
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  // Cleanup 2번째 방법
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+    // setToDos((currentArray) => {
+    //   const newArray = [toDo, ...currentArray];
+    //   console.log(newArray);
+    //   return newArray;
+    // });
+  };
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
