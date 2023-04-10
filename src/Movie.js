a54e523ca54d607e5451e56e22a8cdc17fc2db2c
SeongJin Oh <guk0230@naver.com>
2023-04-10 20:58:07 +0900

    github pages 사용시 빈화면 호출 에러 해결법

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function Movie() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
